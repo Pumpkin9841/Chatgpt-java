6d3727ba6d999d51987d0f8d0b12868c66261f27
zhoufan1 <zhoufan1@xiaohongshu.com>
2023-04-14 13:14:18 +0800

    add EnviromentConstant

---
+++ src/main/java/org/pumpkin/config/EnvironmentConstant.java
@@ -0,0 +1,7 @@
+package org.pumpkin.config;
+
+public class EnvironmentConstant {
+    public static final String HOME = "user.home";
+
+    public static final String CONFIG_PATH = "/.config/revChatGPT/config.json";
+}
