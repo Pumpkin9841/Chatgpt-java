12e56cbfb716744cc47fa9a319c0ed0d5b178367
zhoufan1 <zhoufan1@xiaohongshu.com>
2023-04-14 16:38:58 +0800

    fix add BASE_URL

--- src/main/java/org/pumpkin/config/EnvironmentConstant.java
+++ src/main/java/org/pumpkin/config/EnvironmentConstant.java
@@ -4,4 +4,6 @@ public class EnvironmentConstant {
     public static final String HOME = "user.home";
 
     public static final String CONFIG_PATH = "/.config/revChatGPT/config.json";
+
+    public static final String BASE_URL = "https://bypass.churchless.tech/api/conversation";
 }
